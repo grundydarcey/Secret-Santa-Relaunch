5714a10e772d45bc752e1bb84f9d9f3dac424ebd
Darcey Grundy <grundy.darcey@gmail.com>
2022-02-27 17:47:52 -0500

    displaying member info

--- src/views/Person.vue
+++ src/views/Person.vue
@@ -1,16 +1,17 @@
 <template>
-
     <!-- <v-container>
         one persons page
     </v-container> -->
     <div id='route'>
-        <h1></h1>
+        <h1>{{ member.member_name }}</h1>
+        <h2>{{ lists.link }}</h2>
     </div>
 </template>
 
 <script>
   export default {
-      title: 'single person page',
+      title: 'single',
+      inject: ['$lists'],
       data() {
           return {
               id: this.$route.params.id,
@@ -18,10 +19,17 @@
           }
       },
       created() {
-          this.$http.get('http://localhost:8001/members' + this.id)
-            .then(function(data) {
-                this.member = data.body
+          this.$http.get('http://localhost:8001/members/' + this.id)
+            .then((data) => {
+                this.member = data.data
+                console.log(this.member.member_name)
+                console.log(this.lists[0])
             })
+      },
+      computed: {
+          lists() {
+            return this.$lists();
+          }
       }
   }
 </script>
